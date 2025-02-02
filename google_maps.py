26384266d56c76270fecafc5682496bdb3c78482
AbeborSF <157153416+AbeborSF@users.noreply.github.com>
2025-02-02 18:22:11 +0100

    Create google_maps.py

---
+++ google_maps.py
@@ -0,0 +1,4 @@
+<!-- In your templates, add this inside a script tag -->
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY"></script>
+
+Embed a map showing crime-prone areas.
